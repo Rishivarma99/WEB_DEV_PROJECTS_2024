c71fcc1a4f2748d19d6b2ed8386e6b026179b48a
Rishi <alluririshivarma@gmail.com>
2024-05-23 20:27:41 +0530

    23-may_useEffect_sm_app
    Learned hooks like useEffect usecontext use reducer and updated social media app with  loading using useeffect .and learned fetch data from dummy json

--- WEEK1/JAVA_SCRIPT/3_week/4_Date_class/1_date_basics.js
+++ WEEK1/JAVA_SCRIPT/3_week/4_Date_class/1_date_basics.js
@@ -9,20 +9,24 @@
   ->getFullYear(): we will get the year 
   ->getDay: also 0-indexing and returns a number from 0 to 6 : from Sunday(0) to Saturday(6) 
   ->getTime(): returns in millisections from 1970(fixed time)  : can be used to get the difference between two time objects 
+  ->toLocaleDateString : get in date format
+  ->toLocaleTimeString : to get time format
 
  */
 
- const currentDate = new Date(); 
+const currentDate = new Date();
 
- console.log(currentDate); //2024-02-27T16:44:29.194Z
- 
- console.log(currentDate.getDate());   //27
- console.log(currentDate.getMonth()+1 ); //2
- console.log(currentDate.getFullYear()); //2024
- console.log(currentDate.getDay());  // 2 -> (sunday(0),monday(1),tuesday(2))
+console.log(currentDate); //2024-02-27T16:44:29.194Z
+
+console.log(currentDate.getDate());   //27
+console.log(currentDate.getMonth() + 1); //2
+console.log(currentDate.getFullYear()); //2024
+console.log(currentDate.getDay());  // 2 -> (sunday(0),monday(1),tuesday(2))
 
 
 
- console.log("time in ms since 1970 : ", currentDate.getTime());
+console.log("time in ms since 1970 : ", currentDate.getTime());
 
+console.log(currentDate.toLocaleDateString());
+console.log(currentDate.toLocaleTimeString());
 
---
+++ WEEK21/2_social_media_project/src/components/PostList.jsx
@@ -0,0 +1,40 @@
+import { useCallback, useContext, useEffect, useState } from "react";
+import Post from "./Post";
+import { PostList1 } from "../store/posts-list";
+import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
+
+const PostList = () => {
+  const { postList } = useContext(PostList1);
+  const { addIntialPosts } = useContext(PostList1);
+  const [fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    console.log("fetching started ");
+    setFetching(true);
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((obj) => {
+        addIntialPosts(obj.posts);
+        setFetching(false);
+        console.log("fetching completed");
+      });
+    console.log("fetching returned");
+    return () => {
+      console.log("Cleaning use effect "); // runned when we change the page when the postlist is dead
+    };
+  }, []);
+
+  return (
+    <>
+      {fetching && postList.length === 0 && <LoadingSpinner></LoadingSpinner>}
+      {!fetching && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+
+      {postList.map((post) => (
+        <Post key={post.id} post={post} /> // this is just a return statement so no need {}
+      ))}
+    </>
+  );
+};
+
+export default PostList;
